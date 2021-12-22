8817b76fd6d3ab3c676918f914a7f36e7ba5da70
santi4246 <santiago_pna@hotmail.com>
2021-12-22 12:41:32 -0300

    Update Store Procedure File Alta-Medico

--- Centro Medico/ProyectoInicial/AltaMedico.sql
+++ Centro Medico/ProyectoInicial/AltaMedico.sql
@@ -1,54 +1,33 @@
-ALTER PROC AltaMedico(
-	@nombre varchar(50),
-	@apellido varchar(50),
-	@idEspecialidad int,
-	@descripcion varchar(50)
-)
+ALTER proc ALTA_Medico(
+			@nombre varchar(50),
+			@apellido varchar(20),
+			@idespecialidad int,
+			@descripcion varchar(50)
+			)
 
-AS
+as
+set nocount on
 
-SET NOCOUNT ON --Elimina mensaje de consola de columna afectada
-
-IF NOT EXISTS (SELECT TOP 1 idMedico FROM Medico WHERE nombre = @nombre AND apellido = @apellido)
-
+IF NOT EXISTS(SELECT TOP 1 idmedico from Medico WHERE nombre=@nombre and apellido = @apellido)
 BEGIN
+	
 	INSERT INTO Medico (nombre, apellido)
-	VALUES(@nombre, @apellido)
+	VALUES (@nombre,@apellido)
+		
+	declare @auxIdmedico int
+	set @auxIdmedico = @@IDENTITY
 
-	DECLARE @auxIdMedico int
-	SET @auxIdMedico = @@IDENTITY
+	INSERT INTO MedicoEspecialidad (Idmedico, idespecialidad,descripcion)
+	VALUES (@auxIdmedico, @idespecialidad,@descripcion)
 
-	INSERT INTO MedicoEspecialidad(idMedico, idEspecialidad, descripcion)
-	VALUES(@auxIdMedico, @idEspecialidad, @descripcion)
+	print 'El Médico se agregó correctamente'
+	return
 
-	PRINT 'El Médico ha sido registrado correctamente'
-	RETURN
+	
 END
-
 ELSE
-
 BEGIN
-	PRINT 'El Médico ya existe en los registros'
-	RETURN
+	print 'El Médico ya existe.'
+	return
 END
-
---EXEC AltaMedico 'Gerardo', 'Martínez', 1, 'Médico Residente'
-
-/*
-SELECT * FROM Medico
-SELECT * FROM MedicoEspecialidad
-*/
-
-/*
-SELECT * FROM TablaEjemplo
---Primero es necesario eliminar los registros de la tabla
-DELETE FROM Medico
---Luego, ejecutar el siguiente comando que reinciará los registros comenzando por la P.K.
-dbcc CHECKIDENT ('Medico', RESEED, 0)
---De esta manera se reinicializa el campo Identity
-*/
-
-/*
-INSERT INTO Especialidad VALUES('Clínica Médica')
-SELECT * FROM Especialidad
-*/
+--EXEC ALTA_medico 'Gerardo','Martinez',1,'Medico residente'
