4f6487ba7b91a77dabd181be3fdb60b62f0cd214
santi4246 <santiago_pna@hotmail.com>
2021-12-22 12:43:13 -0300

    Update Store Procedure File Alta-Turno

--- Centro Medico/ProyectoInicial/AltaTurno.sql
+++ Centro Medico/ProyectoInicial/AltaTurno.sql
@@ -1,44 +1,39 @@
-ALTER PROC AltaTurno(
-	@fecha char(14), --20200315	12:00
-	@idPaciente Paciente,
-	@idMedico Medico,
-	@observacion observacion = ''
-)
+ALTER proc ALTA_Turno(
+			@fecha char(14), --20190215 12:00
+			@idpaciente paciente,
+			@idmedico medico,
+			@observacion observacion=''
+			)
 
-AS
+as
 
 /*
-Estado = 0 (Pendiente)
-Estado = 1 (Realizado)
-Estado = 2 (Cancelado)
+ESTADO = 0 (pendiente)
+ESTADO = 1 (realizado)
+ESTADO = 2 (cancelado)
+
 */
+set nocount on
 
-SET NOCOUNT ON --Elimina mensaje de consola de columna afectada
-
-IF NOT EXISTS (SELECT TOP 1 idTurno FROM Turno WHERE fechaTurno = @fecha)
+IF NOT EXISTS(SELECT TOP 1 idturno from Turno WHERE fechaturno=@fecha)
 BEGIN
-	INSERT INTO Turno(fechaTurno, estadoTurno, observacion) --Campos de la tabla
-	VALUES (@fecha, 0, @observacion)--Variables especificados en el Store Procedure
+	INSERT INTO Turno (fechaturno,estadoTurno,observacion)
+	VALUES (@fecha,0,@observacion)
 
-	DECLARE @auxIdTurno Turno
-	SET @auxIdTurno = @@IDENTITY --Comando de sistema que devuelve el último valor insertado en la Tabla Turno
+	declare @auxIdturno turno
+	set @auxIdturno = @@IDENTITY
 
-	INSERT INTO TurnoPaciente(idTurno, idPaciente, idMedico)
-	VALUES (@auxIdTurno, @idPaciente, @idMedico)
+	INSERT INTO TurnoPaciente (idturno,idpaciente,idmedico)
+	VALUES (@auxIdturno,@idpaciente,@idmedico)
 
-	PRINT 'Turno agregado correctamente'
-	RETURN
+	print 'El turno se agregó correctamente'
+	return
+
+	
 END
 ELSE
 BEGIN
-	PRINT 'El turno ya ha sido ingresado'
-	RETURN
+	print 'El turno ya existe.'
+	return
 END
-
---Si el médico no está dado de alta, al cargar un turno arrojará error por la llave foránea
---EXEC AltaTurno '20190215 08:15', 1, 1, 'El paciente tiene que estar en ayunas'
-
-/*
-SELECT * FROM Turno
-SELECT * FROM TurnoPaciente
-*/
+--EXEC ALTA_turno '20190218 09:15', 1, 1,'Observación'
