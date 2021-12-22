e62e0112f0c38314d4348747470045a04d9c1ca2
santi4246 <santiago_pna@hotmail.com>
2021-12-21 21:51:46 -0300

    Update Store Procedure File Alta-Turno

--- Centro Medico/ProyectoInicial/AltaTurno.sql
+++ Centro Medico/ProyectoInicial/AltaTurno.sql
@@ -1,7 +1,7 @@
 ALTER PROC AltaTurno(
 	@fecha char(14), --20200315	12:00
-	@idPaciente paciente,
-	@idMedico medico,
+	@idPaciente Paciente,
+	@idMedico Medico,
 	@observacion observacion = ''
 )
 
@@ -19,10 +19,13 @@ IF NOT EXISTS (SELECT TOP 1 idTurno FROM Turno WHERE fechaTurno = @fecha)
 BEGIN
 	INSERT INTO Turno(fechaTurno, estadoTurno, observacion) --Campos de la tabla
 	VALUES (@fecha, 0, @observacion)--Variables especificados en el Store Procedure
-	DECLARE @auxIdTurno turno
+
+	DECLARE @auxIdTurno Turno
 	SET @auxIdTurno = @@IDENTITY --Comando de sistema que devuelve el último valor insertado en la Tabla Turno
+
 	INSERT INTO TurnoPaciente(idTurno, idPaciente, idMedico)
 	VALUES (@auxIdTurno, @idPaciente, @idMedico)
+
 	PRINT 'Turno agregado correctamente'
 	RETURN
 END
@@ -33,10 +36,9 @@ BEGIN
 END
 
 --Si el médico no está dado de alta, al cargar un turno arrojará error por la llave foránea
-EXEC AltaTurno '20210218 09:15', 2, 1, 'Observacion'
+--EXEC AltaTurno '20190215 08:15', 1, 1, 'El paciente tiene que estar en ayunas'
+
 /*
 SELECT * FROM Turno
 SELECT * FROM TurnoPaciente
-SELECT * FROM Medico
-SELECT * FROM Paciente
 */
