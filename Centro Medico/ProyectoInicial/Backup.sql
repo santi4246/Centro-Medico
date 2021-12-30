84213017297e336d1a86671984f15765f698b125
santi4246 <santiago_pna@hotmail.com>
2021-12-30 12:01:25 -0300

    Update query Backup

--- Centro Medico/ProyectoInicial/Backup.sql
+++ Centro Medico/ProyectoInicial/Backup.sql
@@ -9,3 +9,15 @@ SET @name = 'CentroMedico' + @fecha
 BACKUP DATABASE CentroMedico
 TO DISK = @path
 WITH NO_COMPRESSION, NAME = @name
+
+
+/*
+En algunas versiones de SQL Server 18 y posteriores el componente Server Agent no se encuentra disponible por lo que no se puede ejecutar un proceso JOB. Se puede realizar
+una emulación de este proceso mediante la ejecución de un Script de forma automática. Los pasos son los siguientes:
+1° - Crear una carpeta en el directorio por defecto donde se ejecuta el path (C:\Backup_CentroMedico)
+2° - Dentro de la carpeta se encontrarán dos archivos, el Script con terminación .sql y un archivo .bat que ejecutará el Script con las siguientes sentencias:
+		SQLCMD -s .\192.168.0.7 -i C:\Backup_CentroMedico\Backup.sql
+3° - Una vez realizado los pasos anteriores, se programará una tarea en Windows con el programador de tareas para que ejecute el programa .bat con las especificaciones dadas.
+
+Para que se realicen estas instrucciones se deberá contar con la herramienta cmdsql que se puede descargar de forma gratuita.
+*/
